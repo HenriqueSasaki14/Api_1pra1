57604b71003249d943166f676760469c8329506d
HENRIQUE SASAKI TANNOUS <henrique_tannous@estudante.sesisenai.org.br>
2026-06-30 12:48:02 -0300

    feat: rota post

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import {prisma} from "./lib/prisma.ts"
+import { prisma } from "./lib/prisma.ts"
 
 const app = express();
 const PORT = 3000;
@@ -13,10 +13,29 @@ app.get("/usuarios", async (req, res) => {
     try {
         const users = await prisma.usuario.findMany();
         res.status(200).json(users);
-    } catch (error){
-        res.status(400).json({error: "Não achei os macaquitos"});
+    } catch (error) {
+        res.status(400).json({ error: "Não achei os macaquitos" });
     }
 })
+
+
+app.post("/usuarios", async (req, res) => {
+    const { email, senha, bio, fotoUrl } = req.body;
+    const novoUsuario = await prisma.usuario.create({
+        data: {
+            email,
+            senha,
+            perfil: {
+                create: {
+                    bio,
+                    fotoUrl
+                }
+            },
+            include: { perfil: true }
+        }
+    })
+})
+
 
 app.listen(PORT, () => {
     console.log("Server rodando na porta 3000")

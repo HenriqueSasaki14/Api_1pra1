3bc55155d208aafd172340537191795281f79bcb
HENRIQUE SASAKI TANNOUS <henrique_tannous@estudante.sesisenai.org.br>
2026-07-01 08:48:48 -0300

    Commit com erro na rota patch

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config"
 import { PrismaClient } from "../../generated/prisma/client.ts";
 
-const connectionString = '${process.env.DATABASE_URL}';
+const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaPg({connectionString})
 const prisma = new PrismaClient({adapter})
 
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import { prisma } from "./lib/prisma.ts"
+import { create } from 'node:domain';
 
 const app = express();
 const PORT = 3000;
@@ -20,19 +21,68 @@ app.get("/usuarios", async (req, res) => {
 
 
 app.post("/usuarios", async (req, res) => {
-    const { email, senha, bio, fotoUrl } = req.body;
-    const novoUsuario = await prisma.usuario.create({
-        data: {
-            email,
-            senha,
-            perfil: {
-                create: {
-                    bio,
-                    fotoUrl
+    try {
+        const { email, senha, bio, fotoUrl } = req.body;
+        const novoUsuario = await prisma.usuario.create({
+            data: {
+                email,
+                senha,
+                perfil: {
+                    create: {
+                        bio,
+                        fotoUrl
+                    }
                 }
             },
             include: { perfil: true }
-        }
+        });
+        res.status(200).json(novoUsuario)
+    } catch (error) {
+        res.status(500).json("Erro ao criar o usuário")
+    }
+});
+
+app.put("/usuarios/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const { email, senha, bio, fotoUrl } = req.body;
+
+        const usuarioAtualizado = await prisma.usuario.update({
+            where: { id: Number(id) },
+            data: {
+                email,
+                senha,
+                perfil: {
+                    upsert: {
+                        create: { bio, fotoUrl },
+                        update: { bio, fotoUrl }
+                    }
+                },
+            }
+        });
+        res.status(200).json(usuarioAtualizado);
+    } catch (error) {
+        res.status(400).json({ error: "Erro ao atualizar o usuário", detalhe: error.message });
+    }
+})
+
+app.patch("/usuarios/id:", async (req, res) => {
+    const { id } = req.params
+
+    const { email, senha, bio, fotoUrl } = req.body
+
+    const dadosAtualizados = {
+        email,
+        senha,
+        perfil: (bio || fotoUrl) ? {
+            update: { bio, fotoUrl }
+        } : undefined
+    }
+    Object.keys(dadosAtualizados).forEach(key => dadosAtualizados[key] === undefined && delete dadosAtualizados[key])
+    const usuario = await prisma.usuario.update({
+        where: {id: Number(id)},
+        data: dadosAtualizados,
+        include: (perfil: true)
     })
 })
 
